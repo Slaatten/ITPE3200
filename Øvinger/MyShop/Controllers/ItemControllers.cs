1ed79ba72a86b6ea13d3410325a00d4f70e6c921
stian <stsla0672@oslomet.no>
2025-09-11 11:31:54 +0200

    La til grid og table

--- Øvinger/MyShop/Controllers/ItemControllers.cs
+++ Øvinger/MyShop/Controllers/ItemControllers.cs
@@ -1,41 +1,64 @@
-using System;                                     // Grunnleggende C# funksjoner
-using System.Collections.Generic;                // Gir tilgang til List<T> og andre samlingsklasser
-using System.Linq;                               // For LINQ-spørringer (ikke brukt her, men ofte nyttig)
-using System.Threading.Tasks;                    // For asynkron programmering (ikke brukt her, men standard import)
-using Microsoft.AspNetCore.Mvc;                  // Gir tilgang til Controller og IActionResult (MVC-funksjoner)
-using MyShop.Models;                             // Importerer Item-klassen vi lagde i Models
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using MyShop.ViewModels;
 
-namespace MyShop.Controllers                     // Navnerom for controllere i MyShop
+namespace MyShop.Controllers
 {
-    public class ItemController : Controller     // En controller-klasse som arver fra Controller
+    public class ItemController : Controller
     {
-        public IActionResult Table()             // En action-metode som håndterer "/Item/Table"
+        public IActionResult Table()
         {
-            var items = new List<Item>();        // Lager en liste for å lagre varer
+            var items = GetItems();
+            var itemsViewModel = new ItemsViewModel(items, "Table");
+            return View(itemsViewModel);
+        }
 
-            // Første vare - laget med "ny instans" og deretter satt verdier
-            var item1 = new Item();
-            item1.ItemId = 1;
-            item1.Name = "Pizza";
-            item1.Price = 60;
+        public IActionResult Grid()
+        {
+            var items = GetItems();                   // Henter data fra egen metode
+            var itemsViewModel = new ItemsViewModel(items, "Grid");        // Gir ViewBag beskjed om at dette er tabellvisning
+            return View(itemsViewModel);                       // Sender listen til Grid.cshtml
+        }
 
-            // Andre vare - laget direkte med "object initializer" (kortere måte)
+        public List<Item> GetItems()
+        {
+            var items = new List<Item>();
+
+            var item1 = new Item
+            {
+                ItemId = 1,
+                Name = "Pizza",
+                Price = 150,
+                Description = "Delicious Italian dish with a thin crust topped with tomato sauce, cheese, and various toppings.",
+                ImageUrl = "pizza.jpg"
+            };
+
             var item2 = new Item
             {
                 ItemId = 2,
                 Name = "Fried Chicken Leg",
-                Price = 15
+                Price = 90,
+                Description = "Crispy fried chicken leg with seasoning and spices.",
+                ImageUrl = "fishandchips.jpg"
             };
 
-            // Legger varene inn i listen
+            var item3 = new Item
+            {
+                ItemId = 3,
+                Name = "Tacos",
+                Price = 120,
+                Description = "Mexican style tacos with beef, lettuce, and salsa.",
+                ImageUrl = "tacos.jpg"
+            };
+
+            // Legg til flere varer hvis du vil (4–8) for å fylle ut listen
             items.Add(item1);
             items.Add(item2);
+            items.Add(item3);
 
-            // Sender en tekst til ViewBag som kan vises i View (overskrift)
-            ViewBag.CurrentViewName = "List of Shop Items";
-
-            // Returnerer viewet, og sender med listen items til Table.cshtml
-            return View(items);
+            return items;
         }
     }
 }
